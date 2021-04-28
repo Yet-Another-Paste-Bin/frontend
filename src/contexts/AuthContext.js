bcc833fbbf6e9fce06f4ee1d702994e016cc868d
Tejasvp25 <tejasvp25@gmail.com>
2021-04-28 18:47:11 +0530

    Check if already logged in on Startup

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,10 +3,14 @@ import React, { useState, createContext } from "react";
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
+  const status = localStorage.getItem("id") ? true : false;
+  const authtoken = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+
   const [auth, setAuth] = useState({
-    status: false,
-    username: "",
-    authtoken: "",
+    status,
+    username,
+    authtoken,
   });
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
