f0377c1e5152442c91c6e7c3a703fc6644fbb804
Tejasvp25 <tejasvp25@gmail.com>
2021-06-04 21:42:40 +0530

    MyBins: Show loading msg while requesting to server

--- src/components/MyBins.js
+++ src/components/MyBins.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import { DeleteBin, ReqAllBin } from "../utils/networkUtils";
 import BinItem from "./BinItem";
 import { nanoid } from "nanoid";
+import { BeatLoader } from "react-spinners";
 
 class MyBins extends Component {
-  state = { bins: [] };
+  state = { bins: [], requesting: true };
 
   componentDidMount() {
     if (localStorage.getItem("id") === null) {
       this.props.history.push("/");
     }
-    ReqAllBin().then((res) => {
-      const { data } = res;
-      if (data && data.length > 0) {
-        this.setState({ bins: data });
-      }
-    });
+    ReqAllBin()
+      .then((res) => {
+        const { data } = res;
+        if (data && data.length > 0) {
+          this.setState({ ...this.state, bins: data });
+        }
+      })
+      .finally(() => this.setState({ ...this.state, requesting: false }));
   }
 
   deleteBin = (binId) => {
@@ -46,7 +49,14 @@ class MyBins extends Component {
               textAlign: "center",
             }}
           >
-            <h1> Oops! No Bins Found </h1>
+            {this.state.requesting ? (
+              <>
+                <h1> Loading </h1>
+                <BeatLoader size="20px" loading={true} />
+              </>
+            ) : (
+              <h1> Oops! No Bins Found </h1>
+            )}
           </div>
         )}
       </>
